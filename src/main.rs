fba346a4f6c8faac26f6a416e45073c309296205
Ronald Leppink <ronaldleppink@gmail.com>
2022-12-26 11:51:03 +0100

    Add default, empty window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use bevy::prelude::*;
+
+use bevy_ecs_tilemap::TilemapPlugin;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    window: WindowDescriptor {
+                        title: String::from("Shock Showdown"),
+                        ..default()
+                    },
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
+        .add_plugin(TilemapPlugin)
+        .run();
 }
