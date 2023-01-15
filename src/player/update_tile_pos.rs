35e5b4963589a6f6c24aeb5f430d6ddaec6e1791
Ronald Leppink <ronaldleppink@gmail.com>
2023-01-15 11:29:52 +0100

    Fix player tile updating system

--- src/player/update_tile_pos.rs
+++ src/player/update_tile_pos.rs
@@ -5,27 +5,19 @@ pub fn update_players_tile_pos(
     player_query: Query<(Entity, &Transform), With<Player>>,
 ) {
     for (player_entity, player_transform) in player_query.iter() {
-        update_player_tile_pos(commands, player_entity, player_transform);
+        let player_tile_pos = TilePos::from_world_pos(
+            &player_transform.translation.truncate(),
+            &MAP_SIZE,
+            &TILE_SIZE.into(),
+            &MAP_TYPE,
+        );
+
+        if player_tile_pos.is_none() {
+            continue;
+        }
+
+        commands
+            .entity(player_entity)
+            .insert(player_tile_pos.unwrap());
     }
-}
-
-fn update_player_tile_pos(
-    mut commands: Commands,
-    player_entity: Entity,
-    player_transform: &Transform,
-) {
-    let player_tile_pos = TilePos::from_world_pos(
-        &player_transform.translation.truncate(),
-        &MAP_SIZE,
-        &TILE_SIZE.into(),
-        &MAP_TYPE,
-    );
-
-    if player_tile_pos.is_none() {
-        return;
-    }
-
-    commands
-        .entity(player_entity)
-        .insert(player_tile_pos.unwrap());
 }
